03d4f208f98119f08f1976ebb821d6957baf3a3b
ruusukivi <ruusukivi@gmail.com>
2023-04-30 13:01:40 +0300

    13.1-13.3

--- cli.js
+++ cli.js
@@ -1,7 +1,12 @@
 require('dotenv').config()
 const { Sequelize, QueryTypes } = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DATABASE_URL)
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+      // Your pg options here
+    }
+  });
 
 const main = async () => {
   try {
--- commands.sql
+++ commands.sql
@@ -1,7 +1,7 @@
 CREATE TABLE blogs (
     id SERIAL PRIMARY KEY,
     author text,
-    url text NOT NULL,
+    uri text NOT NULL,
     title text NOT NULL,
     likes int DEFAULT 0
 );
