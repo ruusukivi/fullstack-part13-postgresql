5a8df8790b8b391991ab907b892c1c1c19019e83
ruusukivi <ruusukivi@gmail.com>
2023-04-30 13:36:38 +0300

    SQL fix

--- commands.sql
+++ commands.sql
@@ -6,6 +6,6 @@ CREATE TABLE blogs (
     likes int DEFAULT 0
 );
 
-insert into blogs (author, url, title) values ('Dan Abramov', 'https://overreacted.io/writing-resilient-components/', 'Writing Resilient Components');
-insert into blogs (author, url, title) values ('Martin Fowler', 'https://martinfowler.com/articles/is-quality-worth-cost.html', 'Is High Quality Software Worth the Cost?');
-insert into blogs (author, url, title) values ('Robert C. Martin', 'https://blog.cleancoder.com/uncle-bob/2018/04/13/FPvsOO.html', 'FP vs. OO List Processing');
+insert into blogs (author, uri, title) values ('Dan Abramov', 'https://overreacted.io/writing-resilient-components/', 'Writing Resilient Components');
+insert into blogs (author, uri, title) values ('Martin Fowler', 'https://martinfowler.com/articles/is-quality-worth-cost.html', 'Is High Quality Software Worth the Cost?');
+insert into blogs (author, uri, title) values ('Robert C. Martin', 'https://blog.cleancoder.com/uncle-bob/2018/04/13/FPvsOO.html', 'FP vs. OO List Processing');
