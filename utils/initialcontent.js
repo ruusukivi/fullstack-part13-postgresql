a9f1e7f5cf7f53f136d5e9583febce181e7bd87e
ruusukivi <ruusukivi@gmail.com>
2023-07-17 18:50:50 +0300

    13.17

--- utils/initialcontent.js
+++ utils/initialcontent.js
@@ -1,45 +1,34 @@
-const Blog = require('./blog')
-const User = require('./user')
+const Blog = require('../models/blog')
+const User = require('../models/user')
 
-User.hasMany(Blog)
-Blog.belongsTo(User)
-
-/*const addTestUser = async () => {
-  const users = await User.count();
-  if (users === 0) {
+const addTestUser = async () => {
     const testUser = await User.create({
       username: 'test@test.com',
       name: 'Test User'
     });
-    testUserId = testUser.id
-  }
 }
 const addBlogs = async () => {
-  const count = await Blog.count();
-  if (count === 0) {
     Blog.bulkCreate([{
         author: 'Dan Abramov',
         uri: 'https://overreacted.io/writing-resilient-components/',
         title: 'Writing Resilient Components',
-        userId: testUserId 
+        userId: 1 
       },
       {
         author: 'Martin Fowler',
         uri: 'https://martinfowler.com/articles/is-quality-worth-cost.html',
         title: 'Is quality worth of cost?',
-        userId: testUserId 
+        userId: 1 
       },
       {
         author: 'Robert C. Martin',
         uri: 'https://blog.cleancoder.com/uncle-bob/2018/04/13/FPvsOO.html',
         title: 'FP vs. OO List Processing',
-        userId: testUserId 
+        userId: 1
       }
     ])
-  }
-}*/
+}
 
 module.exports = {
-  Blog,
-  User
+  addBlogs, addTestUser
 }
