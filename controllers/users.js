1c25b60492328a46c07c5f4226e537e64119ac41
ruusukivi <ruusukivi@gmail.com>
2023-07-20 07:53:39 +0300

    13.20

--- controllers/users.js
+++ controllers/users.js
@@ -1,82 +1,93 @@
 const router = require('express').Router()
 
 const {
-  User, Blog
+  User,
+  Blog
 } = require('../models')
 
-router.get('/', async (req, res) => {
-  const users = await User.findAll({
-    include: {
-      model: Blog
-    }
-  })
-  res.json(users)
-})
-
-router.post('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
   try {
-    const user = await User.create(req.body)
-
-    res.json(user)
-  } catch (error) {
-    if (error.message) {
-      const message = error.message
-      return res.status(400).json({
-        message
-      })
-    }
-    return res.status(400).json({
-      error
+    const users = await User.findAll({
+      include: {
+        model: Blog
+      }
     })
+    res.json(users)
+  } catch (error) {
+    next(error)
   }
 })
 
-router.put('/:username', async (req, res) => {
-      try {
-        const user = await User.findOne({
-            where: {
-              username: req.params.username,
-            },
-            include: {
-              model: Blog
-            }
-          }) 
-          if (user) {
-            const updated = await user.update({
-              name: req.body.name
-            })
-            const users = await User.findAll({
-              include: {
-                model: Blog
-              }
-            })
-            res.json(updated)
-          } else {
-            return res.status(404).json({
-              error
-            })
-          }
-        }
-        catch (error) {
-          return res.status(400).json({
-            error
-          })
-        }
-      })
+router.post('/', async (req, res, next) => {
+  try {
+    const user = await User.create(req.body)
+    res.json(user)
+  } catch (error) {
+    next(error)
+  }
+})
 
-    router.get('/:id', async (req, res) => {
-      const id =  req.params.id
-      const user = await User.findByPk({
-        id,
-        include: {
-          model: Blog
-        }
-      })
-      if (user) {
-        res.json(user)
-      } else {
-        res.status(404).end()
+router.put('/:username', async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        username: req.params.username,
+      },
+      include: {
+        model: Blog
       }
     })
+    if (!user) {
+      throw Error('User not found!')
+    }
+    const updated = await user.update({
+      name: req.body.name
+    })
+    const users = await User.findAll({
+      include: {
+        model: Blog
+      }
+    })
+    res.json(updated)
+  } catch (error) {
+    next(error)
+  }
+})
 
-    module.exports = router
+router.get('/:id', async (req, res, next) => {
+  try {
+    const id = req.params.id
+    console.log(id)
+    const user = await User.findByPk(id, {
+      attributes: { exclude: [''] } ,
+      include: [{
+        model: Blog,
+        as: 'readings',
+        attributes: ['id', 'author', 'title', 'uri', 'likes', 'year'],
+        through: { attributes: [] }, 
+      }],
+    })
+    if (!user) {
+      throw Error('User not found!')
+    }
+    res.json(user)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.use((error, req, res, next) => {
+  console.error(error.message)
+  if (error.message === 'User not found!') {
+    return res.status(404).json({
+      error: error.message
+    });
+  } else {
+    console.log(error.message)
+    return res.status(500).json({
+      error: 'Something went wrong. Please try again!'
+    });
+  }
+})
+
+module.exports = router
